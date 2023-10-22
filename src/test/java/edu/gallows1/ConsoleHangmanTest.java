39f6cfb69bc2025152ed5728fbe342319963368b
DmitryProshin <do.proshin@yandex.ru>
2023-10-23 00:31:17 +0500

    add test

---
+++ src/test/java/edu/gallows1/ConsoleHangmanTest.java
@@ -0,0 +1,10 @@
+package edu.gallows1;
+
+import org.junit.jupiter.api.Test;
+
+public class ConsoleHangmanTest {
+    @Test
+    void run(){
+
+    }
+}
---
+++ src/test/java/edu/gallows1/DictionaryTest.java
@@ -0,0 +1,14 @@
+package edu.gallows1;
+
+import edu.gallows1.DictionaryModel.GallowsDictionary;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class DictionaryTest {
+    @Test
+    void randomWord(){
+        String[] words = new String[]{"test", "string"};
+        GallowsDictionary gallowsDictionary;
+        assertThat(new GallowsDictionary().randomWord(words)).isNotEqualTo("");
+    }
+}
