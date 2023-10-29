654679807405d52d7ae4438ebb3451971c25495c
DmitryProshin <do.proshin@yandex.ru>
2023-10-30 01:54:52 +0500

    hw3 add task6

--- src/main/java/edu/hw3/Task6/Stock.java
+++ src/main/java/edu/hw3/Task6/Stock.java
@@ -3,11 +3,19 @@ package edu.hw3.Task6;
 import org.jetbrains.annotations.NotNull;
 
 public class Stock implements Comparable<Stock> {
+    public static String getName() {
+        return name;
+    }
+
+    public static int getCost() {
+        return cost;
+    }
+
     private static String name;
     private static int cost;
 
     @Override
     public int compareTo(@NotNull Stock o) {
-        return 0;
+        return this.getCost() - o.getCost();
     }
 }
--- src/main/java/edu/hw3/Task6/Task6.java
+++ src/main/java/edu/hw3/Task6/Task6.java
@@ -1,4 +1,22 @@
 package edu.hw3.Task6;
 
-public class Task6 {
+import java.util.PriorityQueue;
+
+public class Task6 implements StockMarket {
+    private static PriorityQueue<Stock> priorityQueue = new PriorityQueue<Stock>();
+
+    @Override
+    public void add(Stock stock) {
+        priorityQueue.add(stock);
+    }
+
+    @Override
+    public void remove(Stock stock) {
+        priorityQueue.remove(stock);
+    }
+
+    @Override
+    public Stock mostValuableStock() {
+        return priorityQueue.peek();
+    }
 }
