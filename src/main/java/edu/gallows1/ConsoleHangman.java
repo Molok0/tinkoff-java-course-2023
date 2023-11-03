8b7601366a1c4f8f1b1ae663af6ceacd60b7acd0
DmitryProshin <do.proshin@yandex.ru>
2023-11-03 17:14:40 +0500

    gallows add tests

--- src/main/java/edu/gallows1/ConsoleHangman.java
+++ src/main/java/edu/gallows1/ConsoleHangman.java
@@ -1,6 +1,6 @@
 package edu.gallows1;
 
-import edu.gallows1.DictionaryModel.GallowsDictionary;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -14,11 +14,13 @@ class ConsoleHangman {
     private final List<String> GAME_PROGRESS = new ArrayList<String>();
 
     private Session session;
+    private Scanner scanner;
 
     public static void main(String[] args) {
-        String[] words = new String[]{"text", "word", "type"};
+        String[] words = new String[] {"text", "word", "type"};
         ConsoleHangman consoleHangman =
             new ConsoleHangman(new Session(5, words));
+        consoleHangman.setScanner();
         consoleHangman.run();
     }
 
@@ -27,7 +29,7 @@ class ConsoleHangman {
     }
 
     public void run() {
-        Scanner scanner = new Scanner(System.in);
+
         while (true) {
             GAME_PROGRESS.add(GUESS_MESSAGE);
             System.out.println(GUESS_MESSAGE);
@@ -48,7 +50,6 @@ class ConsoleHangman {
             // Конец игры при победе или поражении
 
             if (guessResult instanceof GuessResult.Win || guessResult instanceof GuessResult.Defeat) {
-                System.out.println(GAME_PROGRESS);
                 break;
             }
         }
@@ -67,5 +68,13 @@ class ConsoleHangman {
         GAME_PROGRESS.add("The word: " + guess.word());
         System.out.println(guess.message());
         System.out.println("The word: " + guess.word());
+    }
+
+    public void setScanner(InputStream inputStream) {
+        this.scanner = new Scanner(inputStream);
+    }
+
+    public void setScanner() {
+        this.scanner = new Scanner(System.in);
     }
 }
---
+++ src/test/java/edu/gallows1/ConsoleHangmanTest.java
@@ -0,0 +1,53 @@
+package edu.gallows1;
+
+import edu.gallows1.DictionaryModel.GallowsDictionary;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ConsoleHangmanTest {
+
+    private static List<String> getGameProgress(List<String> input) {
+        return new ArrayList<>();
+    }
+
+    @Test
+    void testThatGameProgress() {
+        List<String> gameProgress = new ArrayList<>();
+        gameProgress.add("Guess a letter:");
+        gameProgress.add("Missed, mistake 1 out of 5.");
+        gameProgress.add("The word: ****");
+        gameProgress.add("Guess a letter:");
+        gameProgress.add("Hit!");
+        gameProgress.add("The word: t***");
+        gameProgress.add("Guess a letter:");
+        gameProgress.add("Hit!");
+        gameProgress.add("The word: ty**");
+        gameProgress.add("Guess a letter:");
+        gameProgress.add("Hit!");
+        gameProgress.add("The word: typ*");
+        gameProgress.add("Guess a letter:");
+        gameProgress.add("Win!");
+        gameProgress.add("The word: type");
+
+        List<String> input = new ArrayList<>();
+        input.add("w");
+        input.add("t");
+        input.add("y");
+        input.add("p");
+        input.add("e");
+
+        ByteArrayInputStream byteArrayInputStream =
+            new ByteArrayInputStream(String.join(System.lineSeparator(), input).getBytes());
+
+        String[] words = new String[] {"type"};
+        ConsoleHangman consoleHangman =
+            new ConsoleHangman(new Session(5, words));
+        consoleHangman.setScanner(byteArrayInputStream);
+        consoleHangman.run();
+        assertThat(consoleHangman.getGameProgress()).isEqualTo(gameProgress);
+
+    }
+}
