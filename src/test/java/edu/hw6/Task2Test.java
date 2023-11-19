db8affc6d99524db32db41bf1b61a6e8d5458aec
DmitryProshin <do.proshin@yandex.ru>
2023-11-19 16:11:47 +0500

    change test for task2

--- src/test/java/edu/hw6/Task2Test.java
+++ src/test/java/edu/hw6/Task2Test.java
@@ -1,7 +1,6 @@
 package edu.hw6;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.CleanupMode;
 import org.junit.jupiter.api.io.TempDir;
 
 import java.nio.file.Files;
@@ -10,20 +9,19 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class Task2Test {
 
-    @TempDir() Path path;
     private static final String FILE = "Tinkoff Bank Biggest Secret.txt";
     private static final String FILE_COPY = "Tinkoff Bank Biggest Secret — копия.txt";
     private static final String FILE_COPY2 = "Tinkoff Bank Biggest Secret — копия (2).txt";
 
     @Test
-    void cloneFile() {
+    void cloneFile(@TempDir Path path) {
         Path file = Path.of(String.valueOf(path.resolve(FILE)));
         Task2.cloneFile(file);
         assertThat(Files.exists(file)).isEqualTo(true);
     }
 
     @Test
-    void cloneFile1() {
+    void cloneFile1(@TempDir Path path) {
         Path file = Path.of(String.valueOf(path.resolve(FILE)));
         Task2.cloneFile(file);
         Task2.cloneFile(file);
@@ -32,7 +30,7 @@ class Task2Test {
     }
 
     @Test
-    void cloneFile2() {
+    void cloneFile2(@TempDir Path path) {
         Path file = Path.of(String.valueOf(path.resolve(FILE)));
         Task2.cloneFile(file);
         Task2.cloneFile(file);
