db55dfdc0fddcf5fadf60faa0cfd9ecad1123e5c
DmitryProshin <do.proshin@yandex.ru>
2023-12-13 01:04:09 +0500

    add multithread

--- src/main/java/edu/project4/Starter.java
+++ src/main/java/edu/project4/Starter.java
@@ -4,6 +4,7 @@ import edu.project4.Models.AffineCoefficients;
 import edu.project4.Models.FractalImage;
 import edu.project4.Models.Rect;
 import edu.project4.Processors.GammaCorrectionProcessor;
+import edu.project4.Renderers.MultithreadedRenderer;
 import edu.project4.Renderers.SingleThreadedRenderer;
 import edu.project4.Save.ImageFormat;
 import edu.project4.Save.ImageUtils;
@@ -23,6 +24,7 @@ public class Starter {
     private static final double WIDTH_RECT = 1.77;
     private static final double Y_MIN_RECT = -1;
     private static final double HEIGHT_RECT = 1;
+    private static final int THREADS_COUNT = 4;
 
     private Starter() {
     }
@@ -32,6 +34,7 @@ public class Starter {
 
         List<AffineCoefficients> affineCoefficients = AffineTransformation.getCoeff();
         SingleThreadedRenderer renderer = new SingleThreadedRenderer(affineCoefficients);
+        MultithreadedRenderer multithreadedRenderer = new MultithreadedRenderer(affineCoefficients, THREADS_COUNT);
 
         List<Transformation> transformations = List.of(
 //            new Polar()
@@ -42,14 +45,16 @@ public class Starter {
 //            new Swirl()
         );
         Rect world = new Rect(X_MIN_RECT, Y_MIN_RECT, WIDTH_RECT, HEIGHT_RECT);
-        var fractal = renderer.render(canvas, world, transformations, SAMPLES, ITER_PER_SAMPLE, SEED);
         GammaCorrectionProcessor gammaCorrectionProcessor = new GammaCorrectionProcessor();
-        gammaCorrectionProcessor.process(fractal);
+
+        var fractalMul = multithreadedRenderer.render(canvas, world, transformations, SAMPLES, ITER_PER_SAMPLE, SEED);
+        gammaCorrectionProcessor.process(fractalMul);
         ImageUtils.save(
-            fractal,
+            fractalMul,
             Path.of("C:\\Users\\dopro\\IdeaProjects\\java-course-2023\\src\\main\\java\\edu\\project4\\1.bmp"),
             ImageFormat.BMP
         );
+
     }
 
 }
