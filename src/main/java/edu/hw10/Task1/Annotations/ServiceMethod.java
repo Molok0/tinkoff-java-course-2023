151d362edb6d58f1fde619cf0bad70cef141422b
DmitryProshin <do.proshin@yandex.ru>
2023-12-17 23:15:08 +0500

    add task1

---
+++ src/main/java/edu/hw10/Task1/Annotations/ServiceMethod.java
@@ -0,0 +1,13 @@
+package edu.hw10.Task1.Annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Inherited
+@Target({ElementType.METHOD})
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface ServiceMethod {
+}
---
+++ src/main/java/edu/hw10/Task1/TestClass/Cat.java
@@ -0,0 +1,4 @@
+package edu.hw10.Task1.TestClass;
+
+public record Cat(int age, String name, boolean b, char a) {
+}
